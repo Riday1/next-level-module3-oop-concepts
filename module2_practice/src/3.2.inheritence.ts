cbd94cd981227e97d84c08a4008aa22525f570d7
Riday1 <rrriday100@gmail.com>
2024-11-12 17:03:20 +0600

    Student & Teacher are child of Parent class , we drive name,age, address , getSleep fun from parent to child through inheritence

--- module2_practice/src/3.2.inheritence.ts
+++ module2_practice/src/3.2.inheritence.ts
@@ -3,16 +3,16 @@
     // OOP -inheritance (get property + function from parent class )
 
 
-
-    class Student {
+    class Parent {
         name: string;
         age: number;
         address: string;
 
+
         constructor(name: string, age: number, address: string) {
             this.name = name;
             this.age = age;
-            this.address = address;
+            this.address = address
         }
 
         getSleep(numOfHours: number) {
@@ -20,9 +20,49 @@
         }
     }
 
+    class Student extends Parent {
+        // name: string;
+        // age: number;
+        // address: string;
+
+        constructor(name: string, age: number, address: string) {
+
+            // this.name = name;
+            // this.age = age;
+            // this.address = address;
+
+            super(name, age, address)
+        }
+
+
+    }
+
+    class Teacher extends Parent {
+        // name: string;
+        // age: number;
+        // address: string;
+        designation: string;
+
+        constructor(name: string, age: number, address: string, designation: string) {
+            // this.name = name;
+            // this.age = age;
+            // this.address = address;
+            super(name, age, address)
+            this.designation = designation
+
+        }
+
+
+        getClasses(numOfClasses: number) {
+            console.log(`${this.name} will take ${numOfClasses} classes`);
+        }
+
+    }
+
+
     // instance of class Student
-    const student1 = new Student("sohel", 25, "khilkhet")
-    console.log(student1.getSleep(123));
+    // const student1 = new Student("sohel", 25, "khilkhet")
+    // console.log(student1.getSleep(123));
 
 
 
